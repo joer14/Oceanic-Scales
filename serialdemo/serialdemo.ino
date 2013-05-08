926652b42816e7178393eb8a3be6d78e28ff59a4
Joe Rowley <joer14@gmail.com>
2013-05-07 19:46:54 -0700

    Update to serial test

--- serialdemo/serialdemo.ino
+++ serialdemo/serialdemo.ino
@@ -8,7 +8,7 @@
  * your confidence that your lights aren't broken.
  *
  * This code isn't the fastest, but it is just about the simplest you'll
- * find. For performance, investigate code that uses hardware SPI.
+ * find. For performance, inv estigate code that uses hardware SPI.
  *
  * Beware that everyone seems to use different wiring color schemes. I
  * found the order of the wires is more reliable. Looking at the IC side
@@ -34,6 +34,10 @@ void setup() {
   digitalWriteFast(DATA_PIN, LOW);
   digitalWriteFast(CLOCK_PIN, LOW);
   SCmd.addCommand("P",process_command);  // Converts two arguments to integers and echos them back 
+  SCmd.addCommand("party",party);  // Converts two arguments to integers and echos them back 
+  SCmd.addCommand("breath",breath);  // Converts two arguments to integers and echos them back 
+  SCmd.addCommand("id",id);  // Converts two arguments to integers and echos them back 
+  
   SCmd.addDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "What?") 
   Serial.println("Ready"); 
 }
@@ -209,28 +213,223 @@ void process_command()
   if (arg != NULL) 
   {
     aNumber=atol(arg); 
-    Serial.print("Second argument was: "); 
+    Serial.print("Third argument was: "); 
     Serial.println(aNumber); 
     bb = aNumber;
   } 
   else {
-    Serial.println("No second argument"); 
+    Serial.println("No Third argument"); 
   }
   for (uint8_t jj= 0; jj <50; ++jj){
-      set_pixel_rgb(jj,rr, gg,bb);
+      set_pixel_rgb(jj,int(rr), int(gg),int(bb));
+      Serial.println("Setting strip to:");
+      Serial.print(rr);
+      Serial.print(" ,");
+      Serial.print(gg);
+      Serial.print(" ,");
+      Serial.print(bb);
       show();
     }
   
 }
 
+void party()
+{
+  solid_color(simple_color(3) & get_dimmer_mask(100));
+  show();
+  delay(2000);
+  wipe(10);
+  set_pixel(5,simple_color(3) & get_dimmer_mask(100));
+  show();
+  for (uint32_t j = 0; j < 255; ++j) {
+      for (uint32_t n =0; n< LIGHT_COUNT; n++){
+      if (n % 2 == 0){  
+      set_pixel(n,simple_color(1) & get_dimmer_mask(j));
+//      show();
+      }
+      else {
+      set_pixel(n,simple_color(5) & get_dimmer_mask(j));
+//      show();
+      }
+//      show();
+//      n=n+2;
+      }
+      show();
+    }
+    for (uint32_t j = 255; j!= -1; --j) {
+      for (uint32_t n =0; n< LIGHT_COUNT; n++){
+      if (n % 2 == 0){  
+      set_pixel(n,simple_color(1) & get_dimmer_mask(j));
+      
+      }
+      else {
+      set_pixel(n,simple_color(5) & get_dimmer_mask(j));
+     
+      }
+//      show();
+//      n=n+2;
+      }
+      if (j%4 == 0) show();
+      
+    }
+// for (uint8_t k = 0; k < 2; ++k) {
+//    uint32_t color = simple_color(k);
+//    //Dim Up The Given Color
+//    for (uint32_t j = 0; j < 255; ++j) {
+//      for (uint32_t n =0; n< LIGHT_COUNT; n+2){      
+//      set_pixel(n,color & get_dimmer_mask(j));
+//      show();
+//      }
+//    }
+//    //Dim Down the Given Color
+//    for (uint32_t j = 255; j != 0; --j) {
+//      solid_color(color & get_dimmer_mask(j));
+//      show();
+//    }
+//  } 
+}
+
+void set_board_rgb(int r, int g, int b){
+  set_pixel_rgb(15,r,g,b);
+  set_pixel_rgb(17,g,r,b);
+  set_pixel_rgb(19,r,g,b);
+  set_pixel_rgb(21,g,r,b);
+  set_pixel_rgb(23,r,g,b);
+  set_pixel_rgb(25,g,r,b);
+  set_pixel_rgb(27,r,g,b);
+  set_pixel_rgb(29,g,r,b);
+  set_pixel_rgb(31,r,g,b);
+  set_pixel_rgb(34,g,r,b);
+  set_pixel_rgb(36,r,g,b);
+  set_pixel_rgb(39,g,r,b);
+  set_pixel_rgb(43,r,g,b);
+  set_pixel_rgb(46,g,r,b);
+  set_pixel_rgb(49,r,g,b);
+    
+}
+
+int i = 0;
+void breath(){
+ 
+for(i = 15 ; i <= 255; i+=1)
+  { 
+//    solid_color(i);
+    set_board_rgb(i,0,0);
+    
+//    set_pixel_rgb(5,i,0,0);
+//    set_pixel_rgb(7,0,i,0);
+//    set_pixel_rgb(9,i,0,0);
+//    set_pixel_rgb(11,0,i,0);
+     
+    show();
+    if (i > 150) {
+      delay(4);
+    }
+    if ((i > 125) && (i < 151)) {
+      delay(5);
+    }
+    if (( i > 100) && (i < 126)) {
+      delay(7);
+    }
+    if (( i > 75) && (i < 101)) {
+      delay(10);
+    }
+    if (( i > 50) && (i < 76)) {
+      delay(14);
+    }
+    if (( i > 25) && (i < 51)) {
+      delay(18);
+    }
+    if (( i > 1) && (i < 26)) {
+      delay(19);
+    }
+  }
+  for(i = 255; i >=15; i-=1)
+  {
+//    solid_color(i);
+//    set_pixel_rgb(5,i,0,0);
+//    set_pixel_rgb(7,0,i,0);
+//    set_pixel_rgb(9,i,0,0);
+//    set_pixel_rgb(11,0,i,0);
+     set_board_rgb(i,0,0);
+    
+    show();
+    if (i > 150) {
+      delay(4);
+    }
+    if ((i > 125) && (i < 151)) {
+      delay(5);
+    }
+    if (( i > 100) && (i < 126)) {
+      delay(7);
+    }
+    if (( i > 75) && (i < 101)) {
+      delay(10);
+    }
+    if (( i > 50) && (i < 76)) {
+      delay(14);
+    }
+    if (( i > 25) && (i < 51)) {
+      delay(18);
+    }
+    if (( i > 1) && (i < 26)) {
+      delay(19);
+    }
+  }
+  SCmd.readSerial();
+  delay(970);
+  SCmd.readSerial();
+  breath();
+}
+void solid()
+{
+ for (uint8_t k = 0; k < 7; ++k) {
+    uint32_t color = simple_color(k);
+    //Dim Up The Given Color
+    for (uint32_t j = 0; j < 255; ++j) {
+      solid_color(color & get_dimmer_mask(j));
+      show();
+    }
+    //Dim Down the Given Color
+    for (uint32_t j = 255; j != 0; --j) {
+      solid_color(color & get_dimmer_mask(j));
+      show();
+    }
+  } 
+}
+
+void id()    
+{
+  int aNumber;  
+  char *arg; 
+
+//  Serial.println("We're in process_command"); 
+  arg = SCmd.next();
+  int rr,bb,gg =0; 
+  if (arg != NULL) 
+  {
+    aNumber=atoi(arg);    // Converts a char string to an integer
+    Serial.print("First argument was: "); 
+    Serial.println(aNumber); 
+    rr=aNumber;
+  } 
+  else {
+    Serial.println("No arguments"); 
+  }
+  solid_color(0);
+  set_pixel_rgb(rr, 255, 0, 0);
+  show();
+}
 // This gets set as the default handler, and gets called when no other command matches. 
 void unrecognized()
 {
   Serial.println("What?"); 
    for (uint8_t jj= 0; jj <50; ++jj){
-      set_pixel_rgb(jj,255, 0,0);
+      set_pixel_rgb(jj,0, 0,255);
+      set_pixel_rgb(50-jj,255, 255,0);
+      show();
+      solid_color(0);
       show();
     }
 }
-
 
